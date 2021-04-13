54946f5306f7aab59b728506dc88e83fc0d4d88d
edoardo911 <edoardo.stucchi902@gmail.com>
2021-04-13 16:06:31 +0200

    added google maps api and a custom scrollbar

--- src/app/area/area.component.ts
+++ src/app/area/area.component.ts
@@ -5,9 +5,20 @@ import { Router } from "@angular/router";
 @Component({ templateUrl: './area.component.html' })
 export class AreaComponent
 {
+    latitude = 45.4773;
+    longitude = 9.1815;
+    lat = -1;
+    lng = -1;
+
     constructor(private router:Router, private cs:CookieService)
     {
         if(cs.get("username") == "")
             router.navigateByUrl("/login")
+    }
+
+    public onClick(event):void
+    {
+        this.lat = event.coords.lat;
+        this.lng = event.coords.lng;
     }
 }
